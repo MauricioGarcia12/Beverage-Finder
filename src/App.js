a192c501c0430427d16e99a8290f3682dcb29caf
maumainsaw <maumainsaw@gmail.com>
2020-08-31 22:03:51 -0500

    Pasando receta al componente

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Formulario from './components/Formulario'
 import CategoriasProvider from './context/CategoriaContext'
 import RecetasProvider from './context/RecetasContext'
-
+import ListaRecetas from './components/ListaRecetas'
 function App() {
   return (
     <CategoriasProvider>
@@ -14,8 +14,8 @@ function App() {
         <div className='row'>
           <Formulario/>
         </div>
+        <ListaRecetas/>
       </div>
-
       </RecetasProvider>
     </CategoriasProvider>
   );
