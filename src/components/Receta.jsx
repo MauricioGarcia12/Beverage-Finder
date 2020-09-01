5e62386f1094f16256830bc25174569df2c29cc8
maumainsaw <maumainsaw@gmail.com>
2020-08-31 22:09:54 -0500

    Mostrando informacion de las recetas

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -4,6 +4,14 @@ const Receta = ({receta}) => {
     <div className="col-md-4 mb-3">
         <div className="card">
             <h2 className='card-header'>{receta.strDrink}</h2>
+            <img src={receta.strDrinkThumb} alt={`Imagen de ${receta.strDrink}`} className='card-img-top'/>
+            <div className="card-body">
+                <button 
+                type='button'
+                className='btn btn-primary btn-block'
+                >Ver Receta
+                </button>
+            </div>
         </div>
     </div>
      );
